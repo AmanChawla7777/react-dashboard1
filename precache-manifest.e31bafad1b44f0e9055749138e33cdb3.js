f4b0e578563892a47341780c851a82803414f152
AmanChawla7777 <58990088+AmanChawla7777@users.noreply.github.com>
2020-07-28 18:16:34 +0530

    Add files via upload

---
+++ precache-manifest.e31bafad1b44f0e9055749138e33cdb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd0cb4d776700f1d72e065c550ade714",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88084b077dad4c6cebcf",
+    "url": "/static/css/main.0b2ac838.chunk.css"
+  },
+  {
+    "revision": "42290b82bcd12075dd21",
+    "url": "/static/js/2.4d9f2623.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.4d9f2623.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88084b077dad4c6cebcf",
+    "url": "/static/js/main.726dc5bc.chunk.js"
+  },
+  {
+    "revision": "7f56f3a695456023e43e",
+    "url": "/static/js/runtime-main.72a19b3a.js"
+  },
+  {
+    "revision": "6a5b363f1233277f05fb3774519e9307",
+    "url": "/static/media/box.6a5b363f.svg"
+  }
+]);
